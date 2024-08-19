29c59d33d440be44cefd50abd347eb291e38cd31
Brandon Edwards <brandon.edwards144@gmail.com>
2024-08-18 22:18:15 -0400

    added ROUTE hyperparameter

--- src/03-run-point-model.R
+++ src/03-run-point-model.R
@@ -48,10 +48,21 @@ for (i in 1:length(species_list))
                               min_year = 2010,
                               min_n_routes = 1) %>%
     prepare_spatial(strata_map = load_map(st)) %>%
-    prepare_model(model = "gamye", model_variant = "spatial")
+    prepare_model(model = "gamye",
+                  model_file = "models/gamye_spatial_route_hyperparameter.stan", 
+                  model_variant = "spatial")
+  
+  route_original_list <- strsplit(mod_prepped$raw_data$route,
+                             split = "-",
+                             fixed = TRUE)
+  mod_prepped$raw_data$route_original <- paste0(sapply(route_original_list, "[[", 2),
+                           "-",
+                           sapply(route_original_list, "[[", 3))
+  mod_prepped$model_data$route_original <- as.numeric(as.factor(mod_prepped$raw_data$route_original))
+  mod_prepped$model_data$n_route_original <- length(unique(mod_prepped$model_data$route_original))
   
   model_run <- run_model(model_data = mod_prepped,
-                         output_basename = paste0(sp_out, "-point"),
+                         output_basename = paste0(sp_out, "-point_RH"),
                          output_dir = "output/model_runs",
                          overwrite = TRUE)
 }
